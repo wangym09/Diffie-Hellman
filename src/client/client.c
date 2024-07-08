4c8290cd859ef49dfe44fe282ae96ca694762938
minecan-rain <homeminecan@outlook.com>
2024-07-08 11:14:57 +0800

    initial code

---
+++ src/client/client.c
@@ -0,0 +1,5 @@
+#include "../head/head.h"
+
+int main(){
+    
+}
---
+++ src/head/head.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/socket.h>
+
